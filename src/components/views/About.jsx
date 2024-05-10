751013aa8007018754525ca9e80da813bb2aaf4c
JelenaTakac <jelena.andjelic.gr@gmail.com>
2024-05-10 22:33:04 +0200

    Add tailwind utility classes

--- src/components/views/About.jsx
+++ src/components/views/About.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div className="about-page wrapper">
+    <div className="wrapper pt-16 mx-0">
         <h1 className="title about-title">We love <span className="title-stress">shopping</span></h1>
         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo quisquam culpa, labore totam recusandae aliquid! Libero animi adipisci est dicta rem nisi distinctio quia quod aperiam, sed, amet neque similique. Voluptates quasi repellat dolorem placeat eos culpa! Esse rerum exercitationem, obcaecati deserunt incidunt, repellat sint odit quisquam tenetur, blanditiis quidem commodi. Ratione animi soluta corporis deleniti corrupti ut voluptatum similique, nihil cumque, voluptates vitae nesciunt ex mollitia quis quod quasi assumenda provident sed ea.</p>
         <p>Dolorum maiores magni incidunt explicabo sapiente quidem, doloremque assumenda, consequuntur quisquam ab facilis laudantium sed debitis corrupti perferendis laboriosam omnis, ea sunt fugit necessitatibus itaque qui. Ratione animi soluta corporis deleniti corrupti ut voluptatum similique, nihil cumque, voluptates vitae nesciunt ex mollitia quis quod quasi assumenda provident sed ea.</p>
