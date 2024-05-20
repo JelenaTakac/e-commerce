f6b9911387ca443ff41275064cfb6e8fd2df9d87
JelenaTakac <jelena.andjelic.gr@gmail.com>
2024-05-20 22:51:47 +0200

    Update Error component

--- src/components/error/Error.jsx
+++ src/components/error/Error.jsx
@@ -2,9 +2,11 @@ import { ReactComponent as Refres } from "../../assets/Refresh.svg"
 
 const Error = () => {
   return (
-    <div>
-        <h1>Error:</h1>
-        <Refres />
+    <div className="text-center p-12">
+        <h1>Something went wrong...</h1>
+        <div className="mt-12 flex justify-center">
+            <Refres className="w-16 h-1/4 "/>
+        </div>
     </div>
   )
 }
