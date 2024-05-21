59e50e16bd581a418f4bb43b4aac449913063d96
JelenaTakac <jelena.andjelic.gr@gmail.com>
2024-05-21 22:42:52 +0200

    Update Footer component

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,7 +3,7 @@ import "./footer.css"
 
 const Footer = () => {
   return (
-    <div className="footer">
+    <div className="footer-section">
         <p className="copyrights"> © 2024 MyShopStore by Jelena</p>
         <div className="footer-items">
             <Link to={"/privacy-policy"} className="footer-item">Privacy Policy</Link>
